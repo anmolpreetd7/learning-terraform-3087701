bbd7207c006af7b308bc12786f1a1dec3c645acc
anmolpreetd7 <120874954+anmolpreetd7@users.noreply.github.com>
2022-12-20 15:53:42 -0500

    Create dev vpc

--- main.tf
+++ main.tf
@@ -14,8 +14,26 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+##Commenting below code. The new VPC is deployed using module now
+##data "aws_vpc" "default" {
+##  default = true
+##}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "web" {
@@ -34,7 +52,7 @@ module "web_sg" {
   version = "4.16.2"
   name = "web_nsg_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
