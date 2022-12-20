63c7a17b9b41a10440fcdf681bc8667457d81305
anmolpreetd7 <120874954+anmolpreetd7@users.noreply.github.com>
2022-12-20 17:10:12 -0500

    Changed vpc_id in module web_sg

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ module "web_sg" {
   version = "4.16.2"
   name = "web_nsg"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.web_vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
