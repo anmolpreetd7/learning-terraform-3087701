a9c566c59278cf24de98572d631642cb5d82c5e3
anmolpreetd7 <120874954+anmolpreetd7@users.noreply.github.com>
2022-12-20 17:13:41 -0500

    Revert changes

--- main.tf
+++ main.tf
@@ -26,8 +26,8 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  ##public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  public_subnets  = ["10.0.101.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
   enable_nat_gateway = true
 
   tags = {
@@ -52,7 +52,7 @@ module "web_sg" {
   version = "4.16.2"
   name = "web_nsg"
 
-  vpc_id = module.vpc.public_subnets
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
